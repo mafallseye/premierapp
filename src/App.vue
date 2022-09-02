f5d7a2950e016dd199a3975c7a6feaad916cd176
maguette-fall-seye <maguettefseye@gmail.com>
2022-09-02 06:05:02 +0000

    premiere app

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import HelloWorld from "./components/myNavbar.vue";
+import myNavbar from "./components/myNavbar.vue";
 
 export default {
   name: "App",
@@ -18,8 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
