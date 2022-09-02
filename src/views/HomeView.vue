25b69f07a922a2374261c00de0b70500950e5ecc
maguette-fall-seye <maguettefseye@gmail.com>
2022-09-02 08:30:15 +0000

    premiere app1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,11 +23,11 @@
         class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700"
       >
         <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
-         presentation 
+        Je me présente 
         </h5>
         <p class="mb-5   ml-40 mr-40  p-10 text-base text-gray-500 sm:text-lg dark:text-gray-400">
         Je m'appelle Maguette fall SEYE, spécialisée en developpement web et mobile.Passionnée en informatique surtout dans le domaine de 
-        developpement web et mobile je me suis également spécialisée en réseau informatique . 
+        developpement web et mobile je me suis également spécialisée en réseau informatique .Je travail dans le domain de developpement web(fullStack) et mobile dépuis 4 ans 
 
         </p>
         <div
@@ -45,8 +45,8 @@
     <section>
       
 <div class="p-4 bg-gray-100 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-    <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">phase d'introduction aux projets</h5>
-    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Flowbite on iOS &amp; Android. Download the app today.</p>
+
+    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Bon passons aux choses sérieuses</p>
    
 </div>
 
@@ -55,8 +55,8 @@
       <section class="bg-white dark:bg-gray-900">
   <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="max-w-screen-md mb-8 lg:mb-16">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+    
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Voici quelques uns des projets que je suis fiere de vous présenter :</p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div class="border p-8">
