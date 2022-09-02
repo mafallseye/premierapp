8c5d52f658625c7700fa197b74c2bf5905f058cf
maguette-fall-seye <maguettefseye@gmail.com>
2022-09-02 05:30:38 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <myNavbar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/myNavbar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    myNavbar,
+  },
+};
 </script>
 
 <style>
