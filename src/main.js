9df164b96fdb430a1e8b0af925cb170007cdbfef
maguette-fall-seye <maguettefseye@gmail.com>
2022-12-10 16:27:19 +0000

    marche

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './index.css'
-import 'flowbite';
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./index.css";
+import Vue from 'vue'
+
+import "flowbite";
+createApp(App).use(store).use(router).mount("#app");
